22bf2d9c59ed0d7d66f4b48ef0ea71fe7b9cfa87
llynasss <91257160+llynasss@users.noreply.github.com>
2021-09-27 20:03:10 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2.2
+//
+//  Created by Magdalyna Khmyz on 27.09.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
